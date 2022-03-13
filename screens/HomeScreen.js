a36db841c5c04216558003d85c6c7bb0090bb2bb
mridul.kumar <mridul.kumar@procol.in>
2022-03-13 15:43:27 +0530

    added google api key

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,8 @@ import { SafeAreaView, Text, View, Image, StyleSheet } from "react-native";
 import React from "react";
 import tw from "tailwind-react-native-classnames";
 import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_KEY } from "@env";
 
 const HomeScreen = () => {
   return (
@@ -14,6 +16,23 @@ const HomeScreen = () => {
             resizeMode: "contain",
           }}
           source={{ uri: "https://links.papareact.com/gzs" }}
+        />
+        <GooglePlacesAutocomplete
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          placeholder="Where From?"
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={100}
+          query={{
+            key: GOOGLE_MAPS_KEY,
+            language: "en",
+          }}
         />
         <NavOptions />
       </View>
